d66f792a8c95b9bbd890412aa75c7d103087a8c5
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-03-19 09:24:54 -0700

    updated form

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,27 @@
 const router = require('express').Router();
 
+// NEW ROUTE
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+//POST /places
+router.post('/', (req, res) => {
+    console.log(req.body);
+    // if(!req.body.pic){
+    //     //Default image if one is not provided
+    //     req.body.pic = 'http://placekitten.com/400/400'
+    // }
+    // if(!req.body.city){
+    //     req.body.city = 'Anytown'
+    // }
+    // if(!req.body.state){
+    //     req.body.state = 'USA'
+    // }
+    // places.push(req.body);
+    res.redirect('/places');
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -18,6 +40,11 @@ router.get('/', (req, res) => {
     }];
     res.render('places/index', {places});
 });
+
+//HOME ROUTE
+router.get('/', (req, res) => {
+    res.render('places/index', { places });
+})
 
 module.exports = router;
 
