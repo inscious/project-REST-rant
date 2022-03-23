adcd4e705804eff9aa0ca57d0c38398c7f8f69d9
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-03-22 19:47:37 -0700

    added nav

--- controllers/places.js
+++ controllers/places.js
@@ -25,21 +25,21 @@ router.post('/', (req, res) => {
 
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        // pic: '/images/pexels-pixabay-262978.jpg'
-        pic: 'https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        // pic: '/images/pexels-elevate-1267320.jpg'
-        pic: 'https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    }];
+    // let places = [{
+    //     name: 'H-Thai-ML',
+    //     city: 'Seattle',
+    //     state: 'WA',
+    //     cuisines: 'Thai, Pan-Asian',
+    //     // pic: '/images/pexels-pixabay-262978.jpg'
+    //     pic: 'https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+    // }, {
+    //     name: 'Coding Cat Cafe',
+    //     city: 'Phoenix',
+    //     state: 'AZ',
+    //     cuisines: 'Coffee, Bakery',
+    //     // pic: '/images/pexels-elevate-1267320.jpg'
+    //     pic: 'https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+    // }];
     res.render('places/index', {places});
 });
 
--- views/default.jsx
+++ views/default.jsx
@@ -8,7 +8,21 @@ function Def (html) {
                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'/>
                 <link rel='stylesheet' href='/css/style.css'/>
                 <body>
+                    <nav class="nav-bar">
+                        <ul>
+                            <li><a href="/"></a>Home</li>
+                            <li><a href="/places"></a>Places</li>
+                            <li><a href="/places/new"></a>Add place</li>
+                        </ul>
+                    </nav>
                     {html.children}
+                    {/* <footer className='footer'>
+                        <div>
+                            <ul>
+                                <li><a href="https://www.linkedin.com/in/saul-cortes-656b3a22b/">LinkedIn</a></li>
+                            </ul>
+                        </div>
+                    </footer> */}
                 </body>
             </head>
         </html>
