dab70580970dcc4bd9e39aceb8f9353837cab472
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-03-29 22:57:04 -0700

    working delete route for comments, updated id

--- controllers/places.js
+++ controllers/places.js
@@ -17,6 +17,9 @@ router.get('/', (req, res) => {
 
 // Post Route
 router.post('/', (req, res) => {
+    if (req.body.pic === '') { req.body.pic = undefined }
+    if (req.body.city === '') { req.body.city = undefined }
+    if (req.body.state === '') { req.body.state = undefined }
     db.Place.create(req.body)
     .then(() => {
         res.redirect('/places')
@@ -47,10 +50,10 @@ router.get('/new', (req, res) => {
 // Get/Show Route
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
-    .populate('comments')
-    .then(place => {
-        console.log(place.comments)
-        res.render('places/show', { place } )
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments)
+            res.render('places/show', { place })
     })
     .catch(err => {
         console.log('err', err)
@@ -100,150 +103,39 @@ router.get('/:id/edit', (req, res) => {
 // Comment Route
 router.post('/:id/comment', (req, res) => {
     console.log(req.body)
+    if (req.body.author === '') { req.body.author = undefined }
+    req.body.rant = req.body.rant ? true : false
     db.Place.findById(req.params.id)
-    .then(place => {
-        db.Comment.create(req.body)
-        .then(comment => {
-            place.comments.push(comment.id)
-            place.save()
-            .then(() => {
-                res.redirect(`/places/${req.params.id}`)
+        .then(place => {
+            db.Comment.create(req.body)
+            .then(comment => {
+                place.comments.push(comment.id)
+                place.save()
+                    .then(() => {
+                        res.redirect(`/places/${req.params.id}`)
+                    })
+            })
+            .catch(err => {
+                res.render('error404')
             })
         })
         .catch(err => {
             res.render('error404')
         })
-    })
-    .catch(err => {
-        res.render('error404')
-    })
 })
 
 
 // Delete Comment Route
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            console.log('Success')
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 module.exports = router
-
-
-
-
-
-
-
-// //HOME ROUTE
-// router.get('/', (req, res) => {
-//     res.render('places/index', { places });
-// })
-
-
-
-// // NEW ROUTE
-// router.get('/new', (req, res) => {
-//     res.render('places/new')
-// })
-
-
-
-// //POST PLACES
-// router.post('/', (req, res) => {
-//     console.log(req.body)
-//     if (!req.body.pic) {
-//       // Default image if one is not provided
-//         req.body.pic = 'http://placekitten.com/400/400'
-//     }
-//     if (!req.body.city) {
-//         req.body.city = 'Anytown'
-//     }
-//     if (!req.body.state) {
-//         req.body.state = 'USA'
-//     }
-//     places.push(req.body)
-//     res.redirect('/places')
-// })
-
-
-
-// // GET PLACES 
-// router.get('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         res.render('places/show', { place: places[id], id })
-//     }
-// })
-
-
-
-
-// // PUT PLACES ROUTE
-// router.put('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         // Dig into req.body and make sure data is valid
-//         if (!req.body.pic) {
-//             // Default image if one is not provided
-//             req.body.pic = 'http://placekitten.com/400/400'
-//         }
-//         if (!req.body.city) {
-//             req.body.city = 'Anytown'
-//         }
-//         if (!req.body.state) {
-//             req.body.state = 'USA'
-//         }
-
-//         // Save the new data into places[id]
-//         places[id] = req.body
-//         res.redirect(`/places/${id}`)
-//     }
-// })
-
-
-// // DELETE ROUTE
-// router.delete('/places/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         places.splice(id, 1)
-//         res.send('/places')
-//     }
-// })
-
-
-
-// // EDIT ROUTE
-// router.get('/:id/edit', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         res.render('places/edit', { place: places[id] })
-//     }
-// })
-
-
-
-// module.exports = router;
-
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,34 +3,37 @@ const Def = require('../default')
 
 function show (data) {
     let comments = (
-        <h3 className='inactive'>No comments yet!</h3>
+        <h3 className="inactive">No comments yet!</h3>
     )
     let rating = (
-        <h3 className='inactive'>Not yet rated!</h3>
+        <h3 className="inactive">Not yet rated</h3>
     )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐️'
+        }
+        rating = (
+        <h3>{stars} stars</h3>
+        )
         comments = data.place.comments.map(c => {
-            let sumRatings = data.place.comments.reduce((tot, c) => {
-                return tot + c.stars
-            }, 0)
-            let averageRating = Math.round(sumRatings / data.place.comments.length)
-            let stars = ''
-            for (let i = 0; 1 < averageRating; i++) {
-                stars += '⭐️'
-            }
-            rating = (
-                <h3>
-                    {stars} stars
-                </h3>
-            )
             return (
-                <div className="border">
-                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3><stong>- {c.author}</stong></h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
+            <div className="border col-sm-4">
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                    <stong>- {c.author}</stong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+                <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+                    <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                </form>
+            </div>
+        )
         })
     }
     return (
@@ -50,8 +53,8 @@ function show (data) {
                         <h3>{data.place.showEstablished()}</h3>
                         <h4>Serving {data.place.cuisines}</h4>
                         <br />
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
-                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
+                        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit</a>  
+                        <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}> 
                             <button type="submit" className="btn btn-danger">Delete</button>
                         </form>
                     </div>
